283a50a4b46215cb88aac757ac0b121ba8ab7305
Do KyoungGue <86043933+Doku9@users.noreply.github.com>
2022-03-07 04:46:13 +0900

    Update App.js

--- 04_react/src/App.js
+++ 04_react/src/App.js
@@ -23,34 +23,47 @@ function App() {
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
-      <div>
-        <div className="list">
-          <h3>
-            {글제목[0]}
-            <span
-              onClick={() => {
-                따봉변경(따봉 + 1);
-              }}
-            >
-              👍
-            </span>{" "}
-            {따봉}
-          </h3>
-          <p>2월 17일 발행</p>
-          <hr />
 
-          <h3> {글제목[1]} </h3>
-          <p>2월 18일 발행</p>
-          <hr />
+      <div className="list">
+        <h3>
+          {글제목[0]}
+          <span
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}
+          >
+            👍
+          </span>{" "}
+          {따봉}
+        </h3>
+        <p>2월 17일 발행</p>
+        <hr />
 
-          <h3> {글제목[2]} </h3>
-          <p>2월 19일 발행</p>
-          <hr />
-        </div>
+        <h3> {글제목[1]} </h3>
+        <p>2월 18일 발행</p>
+        <hr />
+
+        <h3> {글제목[2]} </h3>
+        <p>2월 19일 발행</p>
+        <hr />
+
         <div>
           <button onClick={제목바꾸기}>버튼</button>
         </div>
       </div>
+
+      <Modal></Modal>
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
