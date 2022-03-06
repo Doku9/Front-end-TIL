2c2c0c05e36fd19a0c81728b7687927a0245715a
Do KyoungGue <86043933+Doku9@users.noreply.github.com>
2022-03-06 21:31:18 +0900

    Update App.js

--- 04_react/src/App.js
+++ 04_react/src/App.js
@@ -3,9 +3,11 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  let [글제목, 글제목변경] = useState("남자 코트 추천");
-
-  let posts = ["강남 고기 맛집", "강북 멋쟁이", "석촌 한강호수"];
+  let [글제목, 글제목변경] = useState([
+    "강남 고기 맛집",
+    "강북 멋쟁이",
+    "석촌 한강호수",
+  ]);
 
   return (
     <div className="App">
@@ -14,14 +16,16 @@ function App() {
       </div>
       <div>
         <div className="list">
-          <h3> {posts} </h3>
+          <h3> {글제목[0]} </h3>
           <p>2월 17일 발행</p>
           <hr />
-          <h3> {posts} </h3>
-          <p>2월 17일 발행</p>
+
+          <h3> {글제목[1]} </h3>
+          <p>2월 18일 발행</p>
           <hr />
-          <h3> {posts} </h3>
-          <p>2월 17일 발행</p>
+
+          <h3> {글제목[2]} </h3>
+          <p>2월 19일 발행</p>
           <hr />
         </div>
       </div>
