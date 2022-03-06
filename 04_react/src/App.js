7089ff01791f31a3e81112c7281f42c5a906b125
Do KyoungGue <86043933+Doku9@users.noreply.github.com>
2022-03-06 13:08:18 +0900

    Update App.js

--- 04_react/src/App.js
+++ 04_react/src/App.js
@@ -1,27 +1,32 @@
 // import logo from './logo.svg';
-import React,{useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [글제목, 글제목변경] = useState("남자 코트 추천");
 
-let [글제목, 글제목변경] = useState('남자 코트 추천');
+  let posts = ["강남 고기 맛집", "강북 멋쟁이", "석촌 한강호수"];
 
-let posts = "강남 고기 맛집"
-
-return (
-<div className="App">
-  <div className='black-nav'>
-    <div>개발 Blog</div>
-  </div>
-  <div>
-    <div className="list">
-      <h3> { posts } </h3>
-      <p>2월 17일 발행</p>
-      <hr />
-         </div>
-  </div>
-</div>
-);
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <div>
+        <div className="list">
+          <h3> {posts} </h3>
+          <p>2월 17일 발행</p>
+          <hr />
+          <h3> {posts} </h3>
+          <p>2월 17일 발행</p>
+          <hr />
+          <h3> {posts} </h3>
+          <p>2월 17일 발행</p>
+          <hr />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
