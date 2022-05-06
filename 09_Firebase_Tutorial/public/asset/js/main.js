73270e0e01dd18d61f25647954f5c9b7c20bfa21
Do KyoungGue <86043933+Doku9@users.noreply.github.com>
2022-05-06 19:02:06 +0900

    load db

--- 09_Firebase_Tutorial/public/asset/js/main.js
+++ 09_Firebase_Tutorial/public/asset/js/main.js
@@ -1,22 +1,27 @@
 const db = firebase.firestore();
+
 db.collection("product")
   .get()
   .then((snapshoot) => {
     snapshoot.forEach((i) => {
-      console.log(i.data());
-      let template = `<div class="product">
+      let template = `
+      <div class="product">
       <div class="thumbnail"></div>
       <div class="flex-gorw-1 p-4">
         <h5 class="title">
-        ${i.data().상품명}
+        ${i.data().제목}
         </h5>
-        <p class="data">2020년 1월 8일</p>
+        <h5>
+        제목
+        </h5>
+        <p class="data">${i.data().날짜}</p>
         <p class="price">
         ${i.data().가격}원
         </p>
-        <p class="float-end">♥ 0</p>
+        <p class="float-end">♥ 1</p>
       </div>
     </div>`;
+
       $(".container").append(template);
     });
   });
