bf1db084a8e97bea9e3ab660e0f59604c3db9d98
Do KyoungGue <86043933+Doku9@users.noreply.github.com>
2022-05-07 17:28:20 +0900

    storage upload function
    스토리지에 이미지 URL과 이미지를 올리는 기능

--- 09_Firebase_Tutorial/public/asset/js/main.js
+++ 09_Firebase_Tutorial/public/asset/js/main.js
@@ -2,26 +2,24 @@ const db = firebase.firestore();
 
 db.collection("product")
   .get()
-  .then((snapshoot) => {
-    snapshoot.forEach((i) => {
+  .then((snapshot) => {
+    snapshot.forEach((i) => {
       let template = `
-      <div class="product">
-      <div class="thumbnail"></div>
-      <div class="flex-gorw-1 p-4">
-        <h5 class="title">
-        ${i.data().제목}
-        </h5>
-        <h5>
-        제목
-        </h5>
-        <p class="data">${i.data().날짜}</p>
-        <p class="price">
-        ${i.data().가격}원
-        </p>
-        <p class="float-end">♥ 1</p>
-      </div>
-    </div>`;
-
+        <div class="product">
+        <div class="thumbnail">
+        <img src=${i.data().이미지} class="thumbnail__img">
+        </div>
+        <div class="flex-gorw-1 p-4">
+          <h5 class="title">
+          ${i.data().제목}
+          </h5>
+          <p class="data">${i.data().날짜}</p>
+          <p class="price">
+          ${i.data().가격}원
+          </p>
+          <p class="float-end">♥ 1</p>
+        </div>
+      </div>`;
       $(".container").append(template);
     });
   });
