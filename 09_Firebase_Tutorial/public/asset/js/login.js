7bf297ab9a09af51092b27fed6df9bb12afff2c7
Do KyoungGue <86043933+Doku9@users.noreply.github.com>
2022-05-07 20:33:39 +0900

    create login
    firebase를 이용한 login 기능 구현

--- 09_Firebase_Tutorial/public/asset/js/login.js
+++ 09_Firebase_Tutorial/public/asset/js/login.js
@@ -1,11 +1,13 @@
 const db = firebase.firestore();
 const storage = firebase.storage();
 
+// ----------  Authentication ----------
 // firebase.auth().createUserWithEmailAndPassword("abc@naver.com", "123456");
 
 $("#register").click(function () {
   let email = $("#email-new").val();
   let password = $("#pw-new").val();
+  let name = $("#name-new").val();
 
   console.log(email);
   console.log(password);
@@ -16,8 +18,24 @@ $("#register").click(function () {
     .then((result) => {
       console.log(result);
       console.log(result.user);
+      result.user.updateProfile({
+        displayName: name,
+      });
     })
     .catch((e) => {
       console.log(e);
+    });
+});
+
+// ---------- login ----------
+$("#login").click(function () {
+  let email = $("#user-email").val();
+  let password = $("#user-pw").val();
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then((result) => {
+      console.log(result.user);
     });
 });
