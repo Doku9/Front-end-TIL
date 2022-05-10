044aa6fc49b362e562f6fef9c6e63859009084cc
Do KyoungGue <86043933+Doku9@users.noreply.github.com>
2022-05-10 20:50:15 +0900

    detail data set db
    db에 detail 정보를 저장

--- 09_Firebase_Tutorial/public/asset/js/detail.js
+++ 09_Firebase_Tutorial/public/asset/js/detail.js
@@ -1,6 +1,12 @@
 const db = firebase.firestore();
 const storage = firebase.storage();
 
+const productData = {
+  participant: [JSON.parse(localStorage.getItem("user")).uid],
+  product: null,
+  date: null,
+};
+
 let queryString = new URLSearchParams(window.location.search);
 console.log(queryString.get("id"));
 
@@ -8,6 +14,9 @@ db.collection("product")
   .doc(queryString.get("id"))
   .get()
   .then((result) => {
+    productData.participant.push(result.data().uid);
+    productData.product = result.data().제목;
+
     $("#detail_img").css({
       "background-image": `url(${result.data().이미지})`,
     });
@@ -29,4 +38,18 @@ db.collection("product")
 //  ----------- Go to edit Page ----------
 $("#edit").click(function () {
   window.location.href = `/edit.html?id=${queryString.get("id")}`;
+});
+
+//  ----------- Go to edit Page ----------
+
+$("#chat").click(function () {
+  productData.date = new Date();
+
+  // ----------create chatroom db
+  db.collection("chatroom")
+    .doc(`${productData.participant[0]}&${productData.participant[1]}`)
+    .set(productData)
+    .then(() => {
+      window.location.href = `/chat.html?buyer=${productData.participant[0]}&seller=${productData.participant[1]}`;
+    });
 });
